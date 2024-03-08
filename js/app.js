e62263d16276662a0beb2cabfcc53f96df977616
lillianlayne <lillianlayne22@gmail.com>
2024-03-08 13:05:59 -0500

    js move tile functionallity checkpoint

--- js/app.js
+++ js/app.js
@@ -1,47 +1,110 @@
+const darkBlack = "#1c1917";
+const darkWhite = "#fafaf9";
+const darkScndWhite = "#aba29e";
+
+let boardSolved;
 let board;
+let colorTileSelection;
+// let bgColor;
+let bgDefaultColor = 'rgba(28,25,23)';
+
 
 // cahce
 const gameBoardDiv = document.getElementById("game-board");
 const colorTilesDiv = document.getElementById("color-tiles");
+const gameContainer = document.getElementById("game-container");
+let colorTile;
 
-// color class
-class Colors {
-  constructor(idx) {
-    this.index = idx;
-    this.color = color;
-  }
+// event listeners 
 
-  getColor() {
-    board.forEach((color, idx) => {
-      color.join(",");
-      return (this.color = `rgba(${color})`);
-    });
-  }
-  getIndex() {
-    board.forEach((color, idx) => {
-      return (this.index = idx);
-    });
-  }
-}
+// gameBoardDiv.addEventListener('click', handlePuzzleBoard);
+// colorTilesDiv.addEventListener('click', handlePuzzleBoard);
+colorTilesDiv.addEventListener('click', handleGetColor);
+// gameContainer.addEventListener('click', handleClick);
 
 // render
 function render() {
   renderBoard();
+  renderColorSelectionDiv();
 }
 
 function renderBoard() {
-    board.forEach((color, idx) => {
-        color.join('');
-        const colorInTile = `rgba(${color})`;
-        const gameBoardIdx = `c${idx}`;
-        const colorTileDiv = document.getElementById(gameBoardIdx)
-        colorTileDiv.style.background = colorInTile;
-        
-    })
+  board.forEach((color, idx) => {
+    color.join("");
+    const colorInTile = `rgba(${color})`;
+    const gameBoardIdx = `c${idx}`;
+    const gameBoardTile = document.getElementById(gameBoardIdx);
+    gameBoardTile.style.background = colorInTile;
+    gameBoardTile.style.border = "none";
+
+    if (color.includes(28 && 25 && 23)) {
+      gameBoardTile.style.border = `1px solid ${darkScndWhite}`;
+    }
+  });
+
 }
 
+function renderColorSelectionDiv() {
+  colorTileSelection = [];
+
+  board.forEach((color, idx) => {
+    if (color.includes(28 && 25 && 23)) {
+        colorTileSelection.push(idx);
+    }
+  });
+
+  colorTileSelection.forEach((color, idx) => {
+    const colorTileId = `g${idx}`;
+    const colorFromBoard = boardSolved[color].join(',');
+    const backgroundColor = `rgba(${colorFromBoard})`;
+    const tileSelectionDiv = document.createElement('div');
+    tileSelectionDiv.id = colorTileId;
+    tileSelectionDiv.style.background = backgroundColor;
+    tileSelectionDiv.classList.add('color-tile');
+    colorTilesDiv.appendChild(tileSelectionDiv);
+
+  })
+  
+}
+
+let state = -1;
+// event listener functions 
+// function handleClick(event) {
+//     let e = event.target;
+//     let bgColor;
+
+//     if (state === -1) {
+//         bgColor = e.style.background;
+//         e.style.border = `1px solid ${darkScndWhite}`;
+//         state *= -1
+//     }
+
+//     if (state === 1) {
+//         e.style.background = bgColor
+//     }
+
+
+
+// }
+
+function handleGetColor(event) {
+    let e = event.target;
+    const bgColor = e.style.background;
+    gameBoardDiv.addEventListener('click', handleFillColor)
+    console.log(bgColor)
+    
+    function handleFillColor(event) {
+        event.target.style.background = bgColor;
+        event.target.style.border = "none";
+        e.style.background = 'none'
+    }
+}
+
+
+
+// init
 function init() {
-  board = [
+  boardSolved = [
     [232, 186, 71],
     [141, 153, 81],
     [63, 127, 84],
@@ -50,6 +113,18 @@ function init() {
     [100, 81, 50],
     [197, 116, 70],
     [151, 66, 40],
+    [110, 16, 16],
+  ];
+
+  board = [
+    [232, 186, 71],
+    [28, 25, 23],
+    [63, 127, 84],
+    [28, 25, 23],
+    [0, 0, 0, 0],
+    [28, 25, 23],
+    [197, 116, 70],
+    [28, 25, 23],
     [110, 16, 16],
   ];
 
