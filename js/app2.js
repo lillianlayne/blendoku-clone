b5cfc57d97b148df23b3438e6c598537668e2ecd
lillianlayne <lillianlayne22@gmail.com>
2024-03-09 13:26:31 -0500

    adding tiles to board fix

--- js/app2.js
+++ js/app2.js
@@ -1,28 +1,93 @@
 // states
 let puzzleKey; // stores what the final puzzle should be
 let guessBoard; // stores the puzzle the user has guessed
+let colorKey;
+let gameState;
+
 let rValues = getRdmValue(245, 47);
 let gValues = getRdmValue(242, 113);
 let bValues = getRdmValue(192, 168);
-
+const defaultValue = "rgba(0,0,0,0)";
 // game state object
 let gamePlay = {
-  getFill: {
-    state: 1,
+  '1': {
     getFill: function () {
       // gets the fill of the selected function
     },
   },
-  setFill: {
-    state: -1,
+  '-1': {
     setFill: function () {
       // sets the fill of the selected function
     },
   },
 };
 
+// cache
+const gameContainerDiv = document.getElementById("game-container");
+const guessContainerDiv = document.getElementById("color-tiles");
 
-// render functions 
+//event listeners 
+gameContainerDiv.addEventListener('click', handleBoard);
+
+function handleBoard(event) {
+  const e = event.target;
+  let previousClick; //stores the previous click
+  let previousBgColor; //stores bgColor of previous
+  let currColor = e.style.background;
+
+  if (e === gameContainerDiv) return;
+  
+  if (gameState === 1) {
+   e.style.border = `3px solid #fafaf9`;
+   previous = e;
+   previousBgColor = e.style.background;
+  }
+
+  if (gameState === -1) {
+    e.style.background = previous.style.background;
+    previous.style.background = currColor;
+    previous.style.border = 'none';
+  }
+
+  gameState *= -1;
+
+
+}
+
+// render functions
+function render() {
+  renderPuzzleKey();
+  renderBoard(puzzleKey);
+  renderGuessTiles();
+}
+
+function renderBoard(puzzleKey) {
+  guessBoard = [
+    [puzzleKey[0][0], defaultValue, defaultValue, puzzleKey[0][3]],
+    [defaultValue, defaultValue, defaultValue, defaultValue],
+    [defaultValue, defaultValue, defaultValue, defaultValue],
+    [puzzleKey[3][0], defaultValue, defaultValue, puzzleKey[3][3]],
+  ];
+  // console.log(guessBoard)
+  guessBoard.forEach((rowArr, rowIdx) => {
+    rowArr.forEach((color, colIdx) => {
+      const divId = `r${rowIdx}c${colIdx}`;
+      const gameTileDiv = document.getElementById(divId);
+      gameTileDiv.style.background = color;
+    });
+  });
+}
+function renderGuessTiles() {
+  const guessTileBoard = puzzleKey.flat();
+  const shuffledArray = guessTileBoard.sort((a, b) => 0.5 - Math.random()); // https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj
+  shuffledArray.forEach((color, idx) => {
+    const gameTileDiv = document.createElement('div');
+    gameTileDiv.classList.add('color-tile');
+    gameTileDiv.style.background = color;
+    guessContainerDiv.appendChild(gameTileDiv);
+  })
+
+}
 
 function getRdmValue(startStep, endStep) {
   const step = Math.floor((startStep - endStep) / 16);
@@ -45,7 +110,12 @@ function renderPuzzleKey() {
       puzzleKey[i][j][2] = bValues.shift();
     }
   }
-  
+  puzzleKey.forEach((rowArr, rowIdx) => {
+    rowArr.forEach((colArr, colIdx) => {
+      return (puzzleKey[rowIdx][colIdx] = `rgba(${colArr})`);
+    });
+    return puzzleKey;
+  });
 }
 
 function init() {
@@ -61,22 +131,22 @@ function init() {
       [0, 0, 0],
       [0, 0, 0],
       [0, 0, 0],
-    ], 
+    ],
     [
       [0, 0, 0],
       [0, 0, 0],
       [0, 0, 0],
       [0, 0, 0],
-    ], 
+    ],
     [
       [0, 0, 0],
       [0, 0, 0],
       [0, 0, 0],
       [0, 0, 0],
-    ], 
+    ],
   ];
-
-  renderPuzzleKey();
+  gameState = 1;
+  render();
 }
 
 init();
