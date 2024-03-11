616fb4adabf8a008a241805f25d040bfc0be1b0e
lillianlayne <lillianlayne22@gmail.com>
2024-03-11 17:54:57 -0400

    forward button functionallity

--- js/instructions.js
+++ js/instructions.js
@@ -5,7 +5,42 @@ const card1 = document.getElementById('intro-1');
 const card2 = document.getElementById('intro-2');
 const card3 = document.getElementById('intro-3');
 
-let cardVisible;
+const cardArr = [card1, card2, card3]
 
-fwdBtn.addEventListener('click', shuffleCardsFwd);
-backBtn.addEventListener('click', shuffleCardsBack);
+let currIdx;
+let nextIdx;
+
+
+fwdBtn.addEventListener('click', handleFwd)
+
+
+
+function handleFwd() {
+    
+
+if (!card1.classList.contains('hide') && card2.classList.contains('hide')) {
+    card2.classList.remove('hide');
+    card1.classList.add('hide')
+} else if (!card2.classList.contains('hide') && card3.classList.contains('hide')) {
+    card3.classList.remove('hide');
+    card2.classList.add('hide')
+} else if (!card3.classList.contains('hide') && card1.classList.contains('hide')) {
+card1.classList.remove('hide');
+    card3.classList.add('hide')
+}
+
+  
+
+
+  console.log(currIdx)
+}
+
+function showStart() {
+    card2.classList.add('hide');
+    card3.classList.add('hide');
+
+    currIdx = 0;
+    console.log(currIdx)
+}
+
+showStart();
