ffb318346b05b9d44e827752743e6c72b9dfff67
lillianlayne <lillianlayne22@gmail.com>
2024-03-11 13:12:32 -0400

    solved puzzle logic

--- js/app.js
+++ js/app.js
@@ -1,110 +1,77 @@
-// states
-let puzzleKey; // stores what the final puzzle should be
-let guessBoard; // stores the puzzle the user has guessed
-let colorKey;
+let puzzKey;
+let puzzBoard;
 let gameState;
-
-let rValues = getRdmValue(232, 110);
-let gValues = getRdmValue(186, 16);
-let bValues = getRdmValue(71, 16);
-
+let guessTileBoard;
 let key1;
 let key2;
 let key3;
 let key4;
+let gameKey1;
+let gameKey2;
+let gameKey3;
+let gameKey4;
+let solved;
 
-const defaultValue = "rgba(0,0,0,0)";
-// game state object
-let gamePlay = {
-  1: {
-    getFill: function () {
-      // gets the fill of the selected function
-    },
-  },
-  "-1": {
-    setFill: function () {
-      // sets the fill of the selected function
-    },
-  },
-};
 
+const gameActions = {
+  getFill: {
+    handleColorDrop: function(event) {
+      previousClick = event;
+
+      event.style.border = '4px solid #1c1917';
+      }
+  }, 
+  setFill: {
+    handleColorDrop: function(event) {
+      event.background = fill;
+      previousClick.style.border = "none";
+    }
+  }
+}
 // cache
-const gameContainerDiv = document.getElementById("game-container");
+const gameBoardDiv = document.getElementById("game-board");
 const guessContainerDiv = document.getElementById("color-tiles");
+const gameBoardTileDiv = document.querySelectorAll("#game-board > div");
 
-//event listeners
-gameContainerDiv.addEventListener("click", handleBoard);
-
-function handleBoard(event) {
-  const e = event.target;
-  let previousClick; //stores the previous click
-  let previousBgColor; //stores bgColor of previous
-  let currColor = e.style.background;
-
-  if (e === gameContainerDiv) return;
-
-  if (gameState === 1) {
-    e.style.border = `3px solid #fafaf9`;
-    previous = e;
-    previousBgColor = e.style.background;
-  }
-
-  if (gameState === -1) {
-    e.style.background = previous.style.background;
-    previous.style.background = currColor;
-    previous.style.border = "none";
-  }
-
-  gameState *= -1;
-}
-
-// render functions
+// event listeners 
+document.getElementById('game-container').addEventListener('click', handleSelection);
+// render
 function render() {
-  renderPuzzleKey();
-  renderBoard(puzzleKey);
+  renderBoard();
   renderGuessTiles();
+  renderControls();
+  getWinner();
+
 }
 
-function renderBoard(puzzleKey) {
-  guessBoard = [
-    [key1, defaultValue, defaultValue, key2],
-    [defaultValue, defaultValue, defaultValue, defaultValue],
-    [defaultValue, defaultValue, defaultValue, defaultValue],
-    [key3, defaultValue, defaultValue, key4],
-  ];
-  // console.log(guessBoard)
-  guessBoard.forEach((rowArr, rowIdx) => {
-    rowArr.forEach((color, colIdx) => {
-      const divId = `r${rowIdx}c${colIdx}`;
-      const gameTileDiv = document.getElementById(divId);
-      gameTileDiv.style.background = color;
-    });
+function renderBoard() {
+  puzzBoard.forEach((color, idx) => {
+    const divId = `r${idx}`;
+    const rgbColor = createRgbColor(color)
+    const gameTileDiv = document.getElementById(divId);
+    gameTileDiv.style.background = rgbColor;
   });
 }
 
 function renderGuessTiles() {
-  const guessTileBoard = puzzleKey.flatMap(keyRow => {
-    return keyRow.filter((color, idx) => {
-      const position = [keyRow.indexOf(color), idx];
-      return !key1.includes(position) && 
-      !key2.includes(position) && 
-      !key3.includes(position) && 
-      !key4.includes(position) 
-
-    })
+  guessTileBoard = puzzKey.filter((val) => {
+    return val !== key1 && val !== key2 && val !== key3 && val !== key4
   })
+  
+  guessTileBoard.sort((a, b) => .5 - Math.random());
+  
+  guessTileBoard.forEach((color, idx) => {
+    const guessTileColor = createRgbColor(color);
+    const guessTileDiv = document.createElement('div');
+    guessTileDiv.classList.add('color-tile');
+    guessTileDiv.style.background = guessTileColor;
+    guessContainerDiv.appendChild(guessTileDiv)
+  })
+  
+  }
 
 
-
-  const shuffledArray = guessTileBoard.sort((a, b) => 0.5 - Math.random()); // https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj
-  shuffledArray.forEach((color, idx) => {
-    const gameTileDiv = document.createElement("div");
-    gameTileDiv.classList.add("color-tile");
-    gameTileDiv.style.background = color;
-    guessContainerDiv.appendChild(gameTileDiv);
-  });
-}
-
+// functions to set up puzzle colors
 function getRdmValue(startStep, endStep) {
   const step = Math.floor((startStep - endStep) / 16);
   const stepArr = [];
@@ -118,56 +85,129 @@ function getRdmValue(startStep, endStep) {
   return stepArr;
 }
 
-function renderPuzzleKey() {
-  for (let i = 0; i < puzzleKey.length; i++) {
-    for (let j = 0; j < puzzleKey[i].length; j++) {
-      puzzleKey[i][j][0] = rValues.shift();
-      puzzleKey[i][j][1] = gValues.shift();
-      puzzleKey[i][j][2] = bValues.shift();
-    }
-  }
-  puzzleKey.forEach((rowArr, rowIdx) => {
-    rowArr.forEach((colArr, colIdx) => {
-      return (puzzleKey[rowIdx][colIdx] = `rgba(${colArr})`);
-    });
-    return puzzleKey;
-  });
+function getPuzzleKey() {
+  const rValues = getRdmValue(201, 61);
+  const gValues = getRdmValue(115, 61);
+  const bValues = getRdmValue(89, 3);
 
-  key1 = puzzleKey[0][0];
-  key2 = puzzleKey[0][3];
-  key3 = puzzleKey[3][0];
-  key4 = puzzleKey[3][3];
+  bValues.sort((a, b) => a - b);
+  rgbArr = [];
+
+  for (let i = 0; i < rValues.length; i++) {
+    let rgb = [rValues[i], gValues[i], bValues[i]];
+    rgbArr.push(rgb);
+  }
+  return rgbArr;
+}
+
+function getWinner() {
+  // create a new array of the background colors of the gameBoard 
+  // compare it to the puzzKey
+  let puzzState = 0;
+  const tileDivArr = [...gameBoardTileDiv];
+  const puzzGuessArr = tileDivArr.map((div) => {
+    let color = div.style.background
+    color = color.replace(/\s/g, '');
+    return color
+  })
+  
+  const puzzKeyArr = puzzKey.map((value) => {
+    return createRgbColor(value)
+  })
+
+for (let i = 0; i < tileDivArr.length; i++) {
+  if (puzzGuessArr[i] === puzzKeyArr[i]) {
+    puzzState += 1
+  }  puzzState += 0
+}
+
+if (puzzState === puzzKeyArr.length) {
+  solved = true
+} solved = false;
+
+}
+
+function createRgbColor(arr) {
+  arr.join(',');
+  return `rgb(${arr})`;
+}
+let previousClick;
+let fill;
+
+
+// event listener functions
+function handleSelection(event) {
+  const e = event.target;
+  const currColor = e.style.background;
+
+  if (gameState === 1) {
+    previousClick = e;
+    fill = e.style.background;
+    e.style.transform = 'scale(.75)'
+  }
+  
+  if (gameState === -1) {
+    previousClick.style.background = currColor;
+    e.style.background = fill;
+    previousClick.style.transform = 'scale(1)';
+
+    
+
+  }
+  console.log(fill)
+  gameState *= -1
 }
 
 function init() {
-  puzzleKey = [
-    [
-      [0, 0, 0],
-      [0, 0, 0],
-      [0, 0, 0],
-      [0, 0, 0],
-    ],
-    [
-      [0, 0, 0],
-      [0, 0, 0],
-      [0, 0, 0],
-      [0, 0, 0],
-    ],
-    [
-      [0, 0, 0],
-      [0, 0, 0],
-      [0, 0, 0],
-      [0, 0, 0],
-    ],
-    [
-      [0, 0, 0],
-      [0, 0, 0],
-      [0, 0, 0],
-      [0, 0, 0],
-    ],
+  puzzKey = [
+    [61, 61, 89],
+    [116, 112, 132],
+    [175, 168, 177],
+    [237, 228, 225],
+    [114, 78, 71],
+    [155, 124, 113],
+    [198, 172, 157],
+    [234, 224, 204],
+    [158, 96, 49],
+    [189, 136, 92],
+    [219, 177, 136],
+    [247, 219, 183],
+    [201, 115, 3],
+    [220, 148, 67],
+    [237, 181, 114],
+    [251, 215, 162],
   ];
-  gameState = 1;
+
+  gameKey1 = 0;
+  gameKey2 = 3;
+  gameKey3 = 12;
+  gameKey4 = 15;
+
+  key1 = puzzKey[0];
+  key2 = puzzKey[3];
+  key3 = puzzKey[12];
+  key4 = puzzKey[15];
+  puzzBoard = [
+      key1, 
+      [],
+      [],
+      key2, 
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      key3, 
+      [],
+      [],
+      key4, 
+  ];
+
   render();
+  gameState = 1;
 }
 
 init();
