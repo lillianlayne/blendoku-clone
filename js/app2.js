c3e091bbb65129ebab7c96cd44fd40943cd14f10
lillianlayne <lillianlayne22@gmail.com>
2024-03-12 21:25:12 -0400

    grid template changes depending on level selected

--- js/app2.js
+++ js/app2.js
@@ -13,114 +13,121 @@ const resetBtn = document.getElementById("reset");
 init();
 
 function init(level) {
-
-    level = 3;
-    puzzKey = getPuzzKey(level);  
+  level = 4;
+  puzzKey = getPuzzKey(level);
   gameState = false;
   render(level);
 }
 
 // color generation functions
-
+// combines values into rgb matrix
 function getPuzzKey(level) {
-    const rVals = initColor(level);
-    const gVals = initColor(level);
-    const bVals = initColor(level);
-    
-let r = rVals.flat();
-let g = gVals.flat();
-let b = bVals.flat();
+  const rVals = initColor(level);
+  const gVals = initColor(level);
+  const bVals = initColor(level);
 
-    let colorMatrix = [];
+  let r = rVals.flat();
+  let g = gVals.flat();
+  let b = bVals.flat();
 
-    for (let i = 0; i < r.length; i++) {
-        const rgbColor = [];
+  let colorMatrix = [];
 
-        rgbColor.push(r[i]);
-        rgbColor.push(g[i]);
-        rgbColor.push(b[i]);
+  for (let i = 0; i < r.length; i++) {
+    const rgbColor = [];
 
-        colorMatrix.push(rgbColor)
-    }
-    return colorMatrix
+    rgbColor.push(r[i]);
+    rgbColor.push(g[i]);
+    rgbColor.push(b[i]);
+
+    colorMatrix.push(rgbColor);
+  }
+  return colorMatrix;
 }
 
+
+// gets a matrix of colors for r, g, or b
 function initColor(level) {
-    const start = getVals(level);
-    const end = getVals(level);
-    const vStart = getBtwnVals(start[0], end[0], level);
-    const vEnd = getBtwnVals(start[level], end[level], level);
-    const valArr = [];
+  const start = getVals(level);
+  const end = getVals(level);
+  const vStart = getBtwnVals(start[0], end[0], level);
+  const vEnd = getBtwnVals(start[level], end[level], level);
+  const valArr = [];
 
+  for (let i = 0; i < start.length; i++) {
+    let btwn = getBtwnVals(vStart[i], vEnd[i], level);
+    valArr.push(btwn);
+  }
 
-    for (let i = 0; i < start.length; i++) {
-        let btwn = getBtwnVals(vStart[i], vEnd[i], level);
-        valArr.push(btwn)
-    }
-
-    return valArr;
+  return valArr;
 }
 
+
+// gets the r,g, or b value in between the start and finish
 function getBtwnVals(start, end, level) {
-    let valStart = start;
-    let step = 0;
-    let valArr = [start];
+  let valStart = start;
+  let step = 0;
+  let valArr = [start];
 
+  if (start > end) {
+    step = Math.floor((start - end) / level);
+  } else {
+    step = Math.floor((end - start) / level);
+  }
+
+  for (let i = 0; i < level; i++) {
     if (start > end) {
-        step = Math.floor((start - end) / level);
+      step = Math.floor((start - end) / level);
+      valArr.push(valStart - step);
+      valStart = valStart - step;
+    } else {
+      step = Math.floor((end - start) / level);
+      valArr.push(valStart + step);
+      valStart = valStart + step;
     }
-    else {
-        step = Math.floor((end - start) / level);
-    }
+  }
 
-    for (let i = 0; i < level; i++) {
-        if (start > end) {
-            step = Math.floor((start - end) / level);
-            valArr.push(valStart - step);
-            valStart = valStart - step
-        }  else {
-            step = Math.floor((end - start) / level);
-            valArr.push(valStart + step);
-            valStart = valStart + step
-        }
-    }
-
-   return valArr 
+  return valArr;
 }
-
+// gets an array of nums for a r, g, or b to initialize the rest 
 function getVals(level) {
-    let start = Math.floor(Math.random() * (255 - 50)) + 50; // Math.floor(Math.random() * (maxNum - minNum that can be thrown)) + minNum rebalance
-    let step = 25 // (startNum / 10 [to make sure the number 2 digits]) + 30 (so the num is higher than 30)
-    const valArray = [start];
-   
-    for (let i = 0; i < level; i ++) {
-        valArray.push(start - step); 
-        start = start - step;
-        step = step + 8
-    }
+  let start = Math.floor(Math.random() * (255 - 50)) + 50; // Math.floor(Math.random() * (maxNum - minNum that can be thrown)) + minNum rebalance
+  let step = 10; // (startNum / 10 [to make sure the number 2 digits]) + 30 (so the num is higher than 30)
+  const valArray = [start];
 
-    return valArray
+  for (let i = 0; i < level; i++) {
+    valArray.push(start - step);
+    start = start - step;
+    step = step + 8;
+  }
+
+  return valArray;
 }
 
 // render functions
 function render(level) {
- levelSetUp(level)
-    // const test = initColor();
-    // console.log(test)
+    renderLevelSetUp(level);
+  // const test = initColor();
+  // console.log(test)
 }
 
-function levelSetUp(level) {
-  
-    let bgColor = puzzKey.map((elem) => {
-        return `rgb(${elem})`
-    })
+function renderLevelSetUp(level) {
+   let gridSize = Math.sqrt(puzzKey.length);
+    let tmpltGrid = `repeat(${gridSize}, 1fr)`
+    gameBoardDiv.style.gridTemplateColumns = tmpltGrid;
+    gameBoardDiv.style.gridTemplateRows = tmpltGrid;
 
-console.log(puzzKey)
-    for (let i = 0; i < bgColor.length; i++) {
-        const gameTileDiv = document.createElement('div');
-        gameTileDiv.classList.add('game-tile');
-        gameTileDiv.style.background = bgColor[i];
-        gameBoardDiv.appendChild(gameTileDiv)
-    }
+//    gameBoardDiv.style.gridTemplateColumns = 
+
+
+
+  let bgColor = puzzKey.map((elem) => {
+    return `rgb(${elem})`;
+  });
+
+  for (let i = 0; i < bgColor.length; i++) {
+    const gameTileDiv = document.createElement("div");
+    gameTileDiv.classList.add("game-tile");
+    gameTileDiv.style.background = bgColor[i];
+    gameBoardDiv.appendChild(gameTileDiv);
+  }
 }
-
